18d5fc93b140784340cf014eea5681165e2a2a8c
OleksandrObydalo <oleksandr.obydalo@nure.ua>
2025-08-03 14:13:18 +0300

    Initial commit

---
+++ src/main/java/org/example/springiocexample/SpringIoCExampleApplication.java
@@ -0,0 +1,13 @@
+package org.example.springiocexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIoCExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringIoCExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/springiocexample/SpringIoCExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springiocexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIoCExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
